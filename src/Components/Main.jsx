9e48cd71fc84f5a42393951215fea31fdff79488
Chadwick Green <chadggreen@gmail.com>
2021-06-15 21:03:55 -0400

    switched button layout

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -62,38 +62,38 @@ function Main() {
 
       <button onClick={operator} value="/">/</button>
 
-      <button onClick={handleInput} value="9">
-        9
+      <button onClick={handleInput} value="7">
+        7
       </button>
       <button onClick={handleInput} value="8">
         8
       </button>
-      <button onClick={handleInput} value="7">
-        7
+      <button onClick={handleInput} value="9">
+        9
       </button>
 
       <button onClick={operator} value="*">*</button>
 
-      <button onClick={handleInput} value="6">
-        6
+      <button onClick={handleInput} value="4">
+        4
       </button>
       <button onClick={handleInput} value="5">
         5
       </button>
-      <button onClick={handleInput} value="4">
-        4
+      <button onClick={handleInput} value="6">
+        6
       </button>
 
       <button onClick={operator} value="+">+</button>
 
-      <button onClick={handleInput} value="3">
-        3
+      <button onClick={handleInput} value="1">
+        1
       </button>
       <button onClick={handleInput} value="2">
         2
       </button>
-      <button onClick={handleInput} value="1">
-        1
+      <button onClick={handleInput} value="3">
+        3
       </button>
 
       <button onClick={operator} value="-">-</button>
